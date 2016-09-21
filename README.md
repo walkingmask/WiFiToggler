a07abaea87bbc3e7c39e72176c6ce62a91e24df1
walkingmask <walkingmask@gmail.com>
2016-09-21 16:37:27 +0900

    v1.0.0
    1. recreate project for delete storyboard
    1. be status bar app
    1. update README

--- README.md
+++ README.md
@@ -6,21 +6,28 @@ Turn Wi-Fi ON/OFF toggler!
 
 ## How to use
 
+# Note
+1. Macのkeycode
+    - http://qiita.com/baba163/items/e2390c4529ec0448151d
+
 ## TODO
-1. Menu bar App化
-    - https://www.raywenderlich.com/98178/os-x-tutorial-menus-popovers-menu-bar-apps
-    - story boardの削除
-        - http://codefromabove.com/2014/09/xcode-6-removing-storyboards-and-creating-useful-empty-projects/
-    - Menu barにアイコン表示
-    - command + Qで消えないGUI
-    - Windowに設定表示
-    - そこでのON/OFF切り替え
-    - 設定を変える？(KeyHolder)
-- sleep前に起動してWiFiを切る
-- sleep復帰時に起動してWiFiを入れる
+- hotkey登録設定(KeyHolder)
+- sleep前に起動してWiFiを切る/sleep復帰時に起動してWiFiを入れる
 - caffeinater機能
     - ショートカットで自動スリープ防止
     - その解除
 
 ## DONE & LOG
 1. アイコン作成
+1. Menu bar App化
+    - https://www.raywenderlich.com/98178/os-x-tutorial-menus-popovers-menu-bar-apps
+    1. ボタンへアイコン画像を適用
+        - 32x32pxじゃないと表示されないっぽい
+        - 32x32pxの画像をassetsにドラッグアンドドロップ
+        - Macの2xに
+        - AppDelegateにtemplate = trueを追加
+    - story boardの削除
+        - http://codefromabove.com/2014/09/xcode-6-removing-storyboards-and-creating-useful-empty-projects/
+        - 変なエラー？が消えなかったのでプロジェクトを作り直してコピペ
+    - Menu barにアイコン表示
+    - GUIなし
--- WiFiToggler/AppDelegate.swift
+++ WiFiToggler/AppDelegate.swift
@@ -7,34 +7,77 @@
 //
 
 import Cocoa
-import Magnet
+import Magnet // hotkey register libraty
 
+@available(OSX 10.10, *) // only for 10.10 or later
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     @IBOutlet weak var window: NSWindow!
-
+    
+    // create app as status item
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // ⌘ + Shift + F5
-        guard let keyCombo = KeyCombo(keyCode: 96, cocoaModifiers: [.CommandKeyMask, .ShiftKeyMask]) else { return }
-        let hotKey = HotKey(identifier: "CommandShiftF5",
-                            keyCombo: keyCombo,
+
+        // status bar button settings
+        if let button = statusItem.button {
+            let img = NSImage(named: "iconblack")
+            img?.template = true
+            button.image = img
+        }
+        
+        // status menu settings
+        let menu = NSMenu()
+        // Turn Wi-Fi Off
+        menu.addItem(NSMenuItem(title: "Turn Wi-Fi Off (⌘⇧F5)", action: #selector(AppDelegate.turnWiFiOff), keyEquivalent: ""))
+        menu.addItem(NSMenuItem.separatorItem())
+        // Turn Wi-Fi On
+        menu.addItem(NSMenuItem(title: "Turn Wi-Fi On  (⌘⇧F6)", action: #selector(AppDelegate.turnWiFiOn), keyEquivalent: ""))
+        menu.addItem(NSMenuItem.separatorItem())
+        // Quit
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.terminate), keyEquivalent: ""))
+        statusItem.menu = menu
+        
+        // hotkey settings
+        // Turn Wi-Fi Off (⌘ + Shift + F5)
+        guard let keyCombo1 = KeyCombo(keyCode: 96, cocoaModifiers: [.CommandKeyMask, .ShiftKeyMask]) else { return }
+        let hotKey1 = HotKey(identifier: "CommandShiftF5",
+                            keyCombo: keyCombo1,
                             target: self,
-                            action: #selector(AppDelegate.tappedHotKey))
-        hotKey.register()
+                            action: #selector(AppDelegate.turnWiFiOff))
+        hotKey1.register()
+        // Turn Wi-Fi On (⌘ + Shift + F6)
+        guard let keyCombo2 = KeyCombo(keyCode: 97, cocoaModifiers: [.CommandKeyMask, .ShiftKeyMask]) else { return }
+        let hotKey2 = HotKey(identifier: "CommandShiftF6",
+                            keyCombo: keyCombo2,
+                            target: self,
+                            action: #selector(AppDelegate.turnWiFiOn))
+        hotKey2.register()
+    }
+    
+    func turnWiFiOff() {
+        let cmd:String = "/bin/bash"
+        let task:NSTask = NSTask()
+        task.launchPath = cmd
+        task.arguments  = ["/Users/walkingmask/Developments/WiFiToggler/src/WiFiToggle.sh"]
+        task.launch()
+    }
+    
+    func turnWiFiOn() {
+        let cmd:String = "/bin/bash"
+        let task:NSTask = NSTask()
+        task.launchPath = cmd
+        task.arguments  = ["/Users/walkingmask/Developments/WiFiToggler/src/WiFiToggle.sh"]
+        task.launch()
+    }
+    
+    func terminate(sender: AnyObject) {
+        NSApplication.sharedApplication().terminate(self)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         HotKeyCenter.sharedCenter.unregisterAll()
-    }
-
-    func tappedHotKey() {
-        let cmd:String = "/bin/bash"
-        let task:NSTask = NSTask()
-        task.launchPath = cmd
-        task.arguments  = ["/Users/walkingmask/Developments/WiFiToggler/WiFiToggle.sh"]
-        task.launch()
-        //print("hotKey!!!!")
     }
 }
 
